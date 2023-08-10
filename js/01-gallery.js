448a2c5a20a7e0b1c1e773d48d85be71085903c3
lukindev <84847710+lukindev@users.noreply.github.com>
2023-08-10 14:46:37 +0200

    1,1

---
+++ js/01-gallery.js
@@ -0,0 +1,48 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+
+function createGalleryItem({ preview, original, description }) {
+  const listItem = document.createElement("li");
+  listItem.classList.add("gallery__item");
+
+  const link = document.createElement("a");
+  link.classList.add("gallery__link");
+  link.href = original;
+
+  const image = document.createElement("img");
+  image.classList.add("gallery__image");
+  image.src = preview;
+  image.setAttribute("data-source", original);
+  image.alt = description;
+
+  link.appendChild(image);
+  listItem.appendChild(link);
+
+  return listItem;
+}
+
+galleryItems.forEach((item) => {
+  const galleryItem = createGalleryItem(item);
+  galleryList.appendChild(galleryItem);
+});
+
+galleryList.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const target = event.target;
+  if (target.classList.contains("gallery__image")) {
+    const largeImageUrl = target.dataset.source;
+    openModal(largeImageUrl);
+  }
+});
+
+function openModal(url) {
+  const instance = basicLightbox.create(
+    `<img src="${url}" width="800" height="600">`
+  );
+  instance.show();
+}
