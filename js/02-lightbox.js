da22f483887931f60a8ced46db9fc5aec40b98c5
lukindev <84847710+lukindev@users.noreply.github.com>
2023-08-10 15:10:37 +0200

    1.4

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+
+function createGalleryItem({ preview, original, description }) {
+  const listItem = document.createElement("li");
+  listItem.classList.add("gallery__item");
+
+  const link = document.createElement("a");
+  link.classList.add("gallery__link");
+  link.href = original;
+
+  const image = document.createElement("img");
+  image.classList.add("gallery__image");
+  image.src = preview;
+  image.alt = description;
+
+  link.appendChild(image);
+  listItem.appendChild(link);
+
+  return listItem;
+}
+
+galleryItems.forEach((item) => {
+  const galleryItem = createGalleryItem(item);
+  galleryList.appendChild(galleryItem);
+});
+
+const lightbox = new SimpleLightbox(".gallery__link", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
